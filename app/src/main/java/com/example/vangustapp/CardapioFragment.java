5ee21d342302f5ccb3bedeab58efa956d338c4cb
Freiregu73 <gustavofreire191@gmail.com>
2026-09-02 19:58:16 -0300

    CardapioFragment feito

--- app/src/main/java/com/example/vangustapp/CardapioFragment.java
+++ app/src/main/java/com/example/vangustapp/CardapioFragment.java
@@ -2,25 +2,24 @@ package com.example.vangustapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CardapioFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class CardapioFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +27,6 @@ public class CardapioFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CardapioFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static CardapioFragment newInstance(String param1, String param2) {
         CardapioFragment fragment = new CardapioFragment();
         Bundle args = new Bundle();
@@ -58,7 +48,59 @@ public class CardapioFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment (aqui já aponta certinho para o fragment_cardapio.xml)
         return inflater.inflate(R.layout.fragment_cardapio, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // 1. Encontrar os RecyclerViews pelo ID correspondente no fragment_cardapio.xml
+        RecyclerView recCarnes = view.findViewById(R.id.idRecCarnes);
+        RecyclerView recFrangos = view.findViewById(R.id.idRecFrangos);
+        RecyclerView recVeganos = view.findViewById(R.id.idRecVeganos);
+        RecyclerView recCombos = view.findViewById(R.id.idRecCombos);
+
+        // 2. Configurar o LayoutManager para horizontal em cada um
+        if (recCarnes != null) {
+            recCarnes.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+            List<ItensCard> listaCarnes = new ArrayList<>();
+            listaCarnes.add(new ItensCard("Brutão na chapa", R.drawable.brutao));
+            listaCarnes.add(new ItensCard("Brutão na chapa", R.drawable.brutao));
+            listaCarnes.add(new ItensCard("Brutão na chapa", R.drawable.brutao));
+            listaCarnes.add(new ItensCard("Brutão na chapa", R.drawable.brutao));
+            recCarnes.setAdapter(new ItensCardAdapter(listaCarnes));
+        }
+
+        if (recFrangos != null) {
+            recFrangos.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+            List<ItensCard> listaFrangos = new ArrayList<>();
+            listaFrangos.add(new ItensCard("Chicken Crispy", R.drawable.frango));
+            listaFrangos.add(new ItensCard("Chicken Crispy", R.drawable.frango));
+            listaFrangos.add(new ItensCard("Chicken Crispy", R.drawable.frango));
+            listaFrangos.add(new ItensCard("Chicken Crispy", R.drawable.frango));
+            recFrangos.setAdapter(new ItensCardAdapter(listaFrangos));
+        }
+
+        if (recVeganos != null) {
+            recVeganos.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+            List<ItensCard> listaVeganos = new ArrayList<>();
+            listaVeganos.add(new ItensCard("Burger Vegano", R.drawable.veggie));
+            listaVeganos.add(new ItensCard("Burger Vegano", R.drawable.veggie));
+            listaVeganos.add(new ItensCard("Burger Vegano", R.drawable.veggie));
+            listaVeganos.add(new ItensCard("Burger Vegano", R.drawable.veggie));
+            recVeganos.setAdapter(new ItensCardAdapter(listaVeganos));
+        }
+
+        if (recCombos != null) {
+            recCombos.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+            List<ItensCard> listaCombos = new ArrayList<>();
+            listaCombos.add(new ItensCard("Combo Família", R.drawable.combo_fogo));
+            listaCombos.add(new ItensCard("Combo Família", R.drawable.combo_fogo));
+            listaCombos.add(new ItensCard("Combo Família", R.drawable.combo_fogo));
+            listaCombos.add(new ItensCard("Combo Família", R.drawable.combo_fogo));
+            recCombos.setAdapter(new ItensCardAdapter(listaCombos));
+        }
     }
 }
